a410dd75b3b891eee9287f997a7197493324510e
NemesisX1 <junior.medehou00@gmail.com>
2023-10-17 12:30:48 +0100

    (fix) port

--- bin/www.ts
+++ bin/www.ts
@@ -18,7 +18,7 @@ bootstrapLogger();
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3100');
 app.set('port', port);
 
 /**
---
+++ src/tests/pupy.test.ts
@@ -0,0 +1,1 @@
+import puppeteer from 'puppeteer';
